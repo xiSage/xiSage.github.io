bae8bcf552f8e0483db393d476e9d7f6bb146602
xiSage <57551864+xiSage@users.noreply.github.com>
2026-03-17 01:38:25 +0800

    button animation

---
+++ scenes/icon_link_buttons/icon_link_button.gd
@@ -0,0 +1,21 @@
+@tool
+class_name CustomButton extends BaseButton
+
+@export var content: Control
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _get_minimum_size() -> Vector2:
+	if content:
+		return content.get_combined_minimum_size()
+	return Vector2.ZERO
+
+
+func _on_mouse_entered() -> void:
+	animation_player.play("hover")
+
+
+func _on_mouse_exited() -> void:
+	animation_player.play_backwards("hover")
+	
+
